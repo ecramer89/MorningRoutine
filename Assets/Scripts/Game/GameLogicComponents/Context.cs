6a7caf2b3d9ca3d02656ddf7f66c52a0a68acc28
Emily Cramer <emily.cramer@adaptechsolutions.ca>
2017-04-02 17:07:14 -0700

    added context gameobjects, with scripts to load in context unique npc and character data

---
+++ Assets/Scripts/Game/GameLogicComponents/Context.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Context : MonoBehaviour {
+	public TextAsset PlaceInfo;
+	public TextAsset NPCInfo;
+	public TextAsset[] CharacterScripts;
+
+
+	public void Load(){
+
+		CreateNPCs();
+	}
+
+	private void CreateNPCs(){
+		string[] NPCData =  NPCInfo.text.Split(new string[]{System.Environment.NewLine}, System.StringSplitOptions.None);
+		for (int i = 0; i < NPCData.Length; i++) {
+			Debug.Log (NPCData [i]);
+		}
+
+	}
+
+}
---
+++ Assets/Scripts/Game/GameLogicComponents/ContextSetter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContextSetter : MonoBehaviour {
+	public GameObject[] contexts;
+	void Start () {
+
+
+		GameState.Instance.playerFetched += () => {
+			//pick a random context, tell it to initialize
+			int random = Random.Range(0, contexts.Length);
+			Context context = contexts[random].GetComponent<Context>();
+			context.Load();
+			//for each other context, unload the assets.
+
+		};
+	}
+	
+
+}
