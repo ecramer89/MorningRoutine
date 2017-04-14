73aafcfcf6fd520aa7deda102504a37cdba8a81f
Emily Cramer <emily.cramer@adaptechsolutions.ca>
2017-04-14 08:32:22 -0700

    removed the game server interface; abstracted into the action creator

--- Assets/Scripts/Game/Actions/ActionCreator.cs
+++ Assets/Scripts/Game/Actions/ActionCreator.cs
@@ -10,7 +10,7 @@ public enum ActionTypes {
 };
 
 public class ActionCreator : MonoBehaviour  {
-	GameServerInterface serverInterface;
+	
 	static ActionCreator instance;
 	public static ActionCreator Instance{
 		get{
@@ -20,27 +20,41 @@ public class ActionCreator : MonoBehaviour  {
 	}
 
 	void Awake(){
-		serverInterface = new GameServerInterface ();
 		instance = gameObject.GetComponent<ActionCreator> ();
 	}
 
 	public void BeginNewGame(){
-		
-		serverInterface.BeginNewGame ();
+		ServerResponse response = PlayerController.BeginGame ();
+		if (!response.error) {
+			ReadModel player = ModelRepository.Get (response.modelName,response.aggregateId);
+			GameReducer.Reduce (ActionTypes.NEW_GAME_BEGUN, player);
+			Debug.Log ($"Success: New Player");
+		} else
+			Debug.Log ("Failed");
 	}
 
 	public void CreateCharacter(int npcId, string npcName, string npcGreeting){
-		
-		serverInterface.CreateCharacter (npcId, npcName, npcGreeting);
+		ServerResponse response = CharacterController.CreateCharacter (npcId, name, npcGreeting);
+		if (!response.error) {
+			ReadModel character = ModelRepository.Get (response.modelName,response.aggregateId);
+			GameReducer.Reduce (ActionTypes.CHARACTER_CREATED, character);
+			Debug.Log ($"Success: New Character {name}");
+		} else
+			Debug.Log ("Failed");
 	}
 
 	public void AddStoryLine(int characterId, int storyLineId, string parentText, string entryPattern, string text, StoryNodeData[] steps){
-		serverInterface.AddStoryLine (characterId, storyLineId, parentText, entryPattern, text, steps);
+		ServerResponse response = CharacterController.AddStoryLine (characterId, storyLineId, parentText, entryPattern, 
+			text, steps);
+		if (!response.error) {
+			Debug.Log ($"Success: New StoryLine");
+		} else
+			Debug.Log ("Failed");
 	}
 
 	public void InitiateDialogue(int characterId){
 		int playerId = GameState.Instance.PlayerState.id;
-		ServerResponse response = serverInterface.InitiateDialogue (characterId, playerId);
+		ServerResponse response = CharacterController.InitiateDialogue (characterId, playerId);
 		if (!response.error) {
 			Debug.Log ($"Success: Initiating dialogue");
 			CharacterReadModel character = (CharacterReadModel)ModelRepository.Get (response.modelName,response.aggregateId);
