62d6d5f5c3e2639c2bd7b0d74232eacb13b808a6
Emily Cramer <emily.cramer@adaptechsolutions.ca>
2017-04-14 08:21:57 -0700

    added a text field

---
+++ Assets/Scripts/Game/UI Components/InputField.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputField : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		gameObject.SetActive (false);
+		GameState.Instance.messageSet += () => {
+			gameObject.SetActive (true);
+		};
+	}
+	
+
+}
