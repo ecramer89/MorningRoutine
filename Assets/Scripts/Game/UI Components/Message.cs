2a5485fd00ce371bbe3c1b7442854b42f7da55a2
Emily Cramer <emily.cramer@adaptechsolutions.ca>
2017-05-14 16:54:44 -0700

    message field is also timed, so offsets two seconds after the dialogue with the character is done

--- Assets/Scripts/Game/UI Components/Message.cs
+++ Assets/Scripts/Game/UI Components/Message.cs
@@ -7,6 +7,8 @@ public class Message : MonoBehaviour {
 
 	Text text;
 	static Message instance;
+	Timed messageDelayBeforeDisappear;
+
 	public static Message Instance{
 		get{
 			return instance;
@@ -18,6 +20,17 @@ public class Message : MonoBehaviour {
 		gameObject.SetActive (false);
 		instance = gameObject.GetComponent<Message> ();
 		text = gameObject.GetComponent<Text> ();
+		messageDelayBeforeDisappear = gameObject.GetComponent<Timed> ();
+		GameState.Instance.characterSet += (id) => {
+			if (id == Constants.NULL_ID) { 
+				messageDelayBeforeDisappear.Set (2);
+				messageDelayBeforeDisappear.Done += () => {
+					gameObject.SetActive (false);
+					text.text="";
+				};
+				messageDelayBeforeDisappear.On ();
+			}
+		};
 
 		GameState.Instance.messageSet+= () => {
 			string newMessage = GameState.Instance.MessageState.message;
